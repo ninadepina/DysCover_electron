0902ac71d81da24e484d363ac23f15983d008282
Nina Vens <nina.vens@hva.nl>
2024-10-27 22:11:41 +0100

    test possibilities

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,39 @@
-console.log('script.js');
-// const HOMEPAGE = 'https://www.google.com';
-// const backButton = document.querySelector('nav__button--back');
-// const forwardButton = document.querySelector('nav__button--forward');
-// console.log(backButton, forwardButton);
+const HOMEPAGE = 'https://www.google.com';
+
+const webview = document.querySelector('#webview');
+const colorButton = document.querySelector('.options__button--color');
+
+colorButton.addEventListener('click', () => {
+	console.log('zoom level:', webview.getZoomLevel());
+	webview.executeJavaScript(`
+    document.querySelectorAll('*').forEach(element => {
+      element.style.color = 'red';
+      element.style.fontFamily = 'Comic Sans MS';
+    });
+  `);
+});
+
+const zoomInButton = document.querySelector('.zoom--in');
+const zoomOutButton = document.querySelector('.zoom--out');
+const zoomPercent = document.querySelector('.zoom--percent');
+
+const updateZoomPercentage = (level) => {
+	const scale = Math.pow(1.2, level);
+	const percentage = Math.round(scale * 100);
+	zoomPercent.textContent = `${percentage}%`;
+};
+
+let currentZoomLevel = 0;
+updateZoomPercentage(currentZoomLevel);
+
+zoomInButton.addEventListener('click', () => {
+	currentZoomLevel += 1;
+	webview.setZoomLevel(currentZoomLevel);
+	updateZoomPercentage(currentZoomLevel);
+});
+
+zoomOutButton.addEventListener('click', () => {
+	currentZoomLevel -= 1;
+	webview.setZoomLevel(currentZoomLevel);
+	updateZoomPercentage(currentZoomLevel);
+});
