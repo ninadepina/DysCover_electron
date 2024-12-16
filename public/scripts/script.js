4e352e600fd0dafb3bb4679e25c08a371b6f49dc
Nina Vens <nina.vens@hva.nl>
2024-12-16 20:15:32 +0100

    fix opening dropdown

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,21 @@
-const HOMEPAGE = 'https://www.google.com';
+// keep dropdown open when clicking button
+document.addEventListener('DOMContentLoaded', () => {
+	const optionButtons = document.querySelectorAll('.option-button');
+	const optionContents = document.querySelectorAll('.option-content');
 
-const webview = document.querySelector('#webview');
+	optionButtons.forEach((button) => {
+		button.addEventListener('click', (e) => {
+			const content = button.nextElementSibling;
+			content.classList.toggle('option-content--visible');
+			e.stopPropagation();
+		});
+	});
+
+	document.addEventListener('click', (e) => {
+		optionContents.forEach((content) => {
+			if (!content.contains(e.target)) {
+				content.classList.remove('option-content--visible');
+			}
+		});
+	});
+});
